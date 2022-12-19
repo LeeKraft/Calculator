261909a11fabe3634444b776817a9032996651b3
Larry <zhenya.kraft.2000@mail.ru>
2022-12-19 13:15:53 +0300

    add script

---
+++ calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def calculate(a, b, operation):
+    result = None
+    if operation == '+':
+        result = a + b
+    elif operation == '-':
+        result = a - b
+    elif operation == '*':
+        result = a * b
+    elif operation == '/':
+        result = a / b
+    else:
+        result = "Unknown operation"
+    return result
+
+def ask_operation():
+    operation = input()
+    return operation
+
+def run_calculator():
+    a = int(input())
+    b = int(input())
+    operation = ask_operation()
+    result = calculate(a, b, operation)
+    print(result)
+
+run_calculator()
